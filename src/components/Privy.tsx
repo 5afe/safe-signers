cc7657c4dd2f7cc7275dab7409b14bbf2878e5a9
Germán Martínez <germaga92@gmail.com>
2024-08-14 19:31:30 +0100

    Add Privy component

---
+++ src/components/Privy.tsx
@@ -0,0 +1,87 @@
+import privyLogo from '@/assets/privy_logo.png'
+import { PrivyProvider, usePrivy, useWallets } from '@privy-io/react-auth'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { WalletClient, createWalletClient, custom } from 'viem'
+import { sepolia } from 'viem/chains'
+
+// Get the APP_ID from the Privy dashboard.
+const APP_ID = ''
+
+function PrivyApp() {
+  const { login, logout, ready, authenticated } = usePrivy()
+  const { ready: readyWallets, wallets } = useWallets()
+  const [provider, setProvider] = useState<WalletClient | null>(null)
+  const [signerAddress, setSignerAddress] = useState<string | null>(null)
+
+  useEffect(() => {
+    const init = async () => {
+      if (ready && authenticated && readyWallets && wallets.length > 0 ) {
+        const ethereumProvider = await wallets[0].getEthereumProvider()
+        const client = createWalletClient({
+          chain: sepolia,
+          transport: custom(ethereumProvider)
+        })
+  
+        setProvider(client)
+        setSignerAddress(wallets[0].address)
+      }
+    }
+    init()
+  }, [ready, authenticated, readyWallets, wallets])
+  
+  const connect = async () => {
+    try {
+      login()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const disconnect = async () => {
+    logout()
+
+    setProvider(null)
+    setSignerAddress(null)
+  }
+
+  const unloggedInView = (
+    <button onClick={connect}>
+      Connect
+    </button>
+  )
+
+  const loggedInView = (
+    <>
+      <button onClick={disconnect}>
+        Disconnect
+      </button>
+    </>
+  )
+
+  return (
+    <div className="card">
+      <div className="title">
+        <Image src={privyLogo} alt="Magic" height="30" />
+        <h2>Privy</h2>
+      </div>
+      <pre>{signerAddress || 'Not connected'}</pre>
+      {signerAddress ? loggedInView : unloggedInView}
+    </div>
+  )
+}
+
+export default function PrivyComponent() {
+  return (
+    <PrivyProvider
+      appId={APP_ID}
+      config={{
+        embeddedWallets: { 
+          createOnLogin: 'users-without-wallets' // defaults to 'off'
+        }
+      }}
+    >
+      <PrivyApp />
+    </PrivyProvider>
+  )
+}
